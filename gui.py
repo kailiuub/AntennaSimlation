475d57f9b8f87e57e6f4b04482da70c9c6026790
kailiuub <kailiu@buffalo.edu>
2017-02-23 22:05:42 -0500

    finished tab2-5 for code display

--- gui.py
+++ gui.py
@@ -1,32 +1,208 @@
-from necpp import *
-import math
+import sys
+import csv
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
 
-# import user modules
-import impedance
-nl=100 # number of scanned points for length
-nr=50 # number of scanned points for radius
-lengths=np.linspace(0.2, 5.0, nl)
-radii=np.linspace(0.002, 0.005, nr)
-z0=50 # instrinsic impedance
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import math
 
-reflections=np.zeros((nl,nr))
+import plot
 
-for i in range(nl):
-  for j in range(nr):
-    z = impedance.impedance(freq=134.5, base=0.5, length=lengths[i], radius=radii[j])
-    reflections[i,j]=impedance.gamma(z,z0)
+# **** Implement Algorithms ****
+class FF (FigureCanvas):
+    def __init__(self,parent=None):
+        self.nl=20 # number of scanned points for length
+        self.nr=10 # number of scanned points for radius
+        self.l0=0.2  # start of scanned length
+        self.l1=5.0  # end of scanned length
+        self.r0=0.002  # start of scanned radius
+        self.r1=0.01  # end of scanned radius
+        lengths=np.linspace(self.l0, self.l1, self.nl)
+        radii=np.linspace(self.r0, self.r1, self.nr)
+        self.z0=50.0 # instrinsic impedance
+        reflections=np.zeros((self.nr,self.nl))
+        reflections=plot.compute(self.nl,self.nr,lengths,radii,self.z0)
+        self.fig1, self.ax1, self.ax2, self.cb1, self.cb2, self.axcb1, self.axcb2=plot.plot(lengths, radii, reflections)
+        FigureCanvas.__init__(self,self.fig1)
+        self.setParent(parent) 
+    def updatefig(self):
+        lengths=np.linspace(self.l0, self.l1, self.nl)
+        radii=np.linspace(self.r0, self.r1, self.nr)
+        L, R = np.meshgrid(lengths, radii)
+        reflections=np.zeros((self.nr,self.nl))
+        reflections=plot.compute(self.nl,self.nr,lengths,radii,self.z0)
+        self.ax1.cla()    # clear plots and axis
+        self.ax2.cla()
+        #c=self.ax1.contourf(L, R, reflections, cmap=cm.plasma)    # replot the figures
+        c=self.ax1.imshow(reflections, origin="lower", interpolation="bilinear", extent=[np.amin(lengths),np.amax(lengths),np.amin(radii),np.amax(radii)], cmap=cm.plasma, aspect=(np.amax(lengths)-np.amin(lengths))/(np.amax(radii)-np.amin(radii)) )
+        s=self.ax2.plot_surface(L, R, reflections, cmap=cm.plasma)
+        self.ax1.set_xlabel("Antenna length (m)")
+        self.ax1.set_ylabel("Radius (m)")
+        self.ax2.set_xlabel("Antenna length (m)")
+        self.ax2.set_ylabel("Radius (m)")
+        self.ax2.set_zlabel("Reflection Coefficient")
+        # update colorbars, just simply replot colorbar
+        self.cb1=plt.colorbar(c, cax=self.axcb1)
+        self.cb2=plt.colorbar(s, cax=self.axcb2)
+        self.draw()
+        
+
+class App(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setWindowTitle('Channel Flow - Navier-Stokes')
+        self.setGeometry(50,50,1150,600)
+
+        # **** Create Menu ****
+        self.menu=self.menuBar()
+        self.file=self.menu.addMenu('File')
+        self.edit=self.menu.addMenu('Edit')
+        self.help=self.menu.addMenu('Help')
+
+        self.exit=QAction(QIcon(''),'Exit',self)
+        self.exit.setShortcut('Ctrl+Q')
+        self.exit.triggered.connect(self.close)
+        self.file.addAction(self.exit)
+
+        self.about=QAction(QIcon(''),'About',self)
+        self.about.setShortcut('Ctrl+T')
+        self.about.triggered.connect(self.aboutact)
+        self.help.addAction(self.about)
+
+        # **** Create 4 Tabs to show Equations, Codes and GUI ****
+        self.mainW1=QWidget(self)
+        self.layout1=QVBoxLayout(self.mainW1)
+        
+        # initiate tabs
+        self.tabs=QTabWidget()
+        self.tab1=QWidget()
+        self.tab2=QWidget()
+        self.tab3=QWidget()
+        self.tab4=QWidget()
+        self.tab5=QWidget()
+        # add tab1 and tab2 to tabs
+        self.tabs.addTab(self.tab1,'Introduction')
+        self.tabs.addTab(self.tab2,'Code 1: NECPP')
+        self.tabs.addTab(self.tab3,'Code 2: GUI INIT')
+        self.tabs.addTab(self.tab4,'Code 3: GUI')
+        self.tabs.addTab(self.tab5,'GUI')
+	
+        # create tab1 for intro with pdf file scroll bar      
+        
+        self.tab1_layout=QVBoxLayout()
+        self.textedit0=QPlainTextEdit(readOnly=True)
+        self.text0=open('README.md').read()
+        self.textedit0.setPlainText(self.text0)
+        self.tab1_layout.addWidget(self.textedit0)
+        self.tab1.setLayout(self.tab1_layout)
+        
+        # create tab2 for Code1 
+        self.tab2_layout=QVBoxLayout()
+        self.textedit1=QPlainTextEdit(readOnly=True)
+        self.text1=open('impedance.py').read()
+        self.textedit1.setPlainText(self.text1)
+        self.tab2_layout.addWidget(self.textedit1)
+        self.tab2.setLayout(self.tab2_layout)
+
+        # create tab3 for Code2 
+        self.tab3_layout=QVBoxLayout()
+        self.textedit2=QPlainTextEdit(readOnly=True)
+        self.text2=open('plot.py').read()
+        self.textedit2.setPlainText(self.text2)
+        self.tab3_layout.addWidget(self.textedit2)
+        self.tab3.setLayout(self.tab3_layout)
+       
+        # create tab4 for Code3 
+        self.tab4_layout=QVBoxLayout()
+        self.textedit3=QPlainTextEdit(readOnly=True)
+        self.text3=open('gui.py').read()
+        self.textedit3.setPlainText(self.text3)
+        self.tab4_layout.addWidget(self.textedit3)
+        self.tab4.setLayout(self.tab4_layout)
+
+        # create tab5 for GUI  (input textboxs and button and results)
+        self.tab5_layout=QGridLayout()
+        self.fig=FF()
+        self.tab5_layout.addWidget(self.fig,0,0,6,6)
+        self.label2=QLabel('number of steps of length(nl)')
+        self.label3=QLabel('start of length (l0)')
+        self.label4=QLabel('end of length (l1)')
+        self.label5=QLabel('number of steps of radius (nr)')
+        self.label6=QLabel('start of radius (r0)')
+        self.label7=QLabel('end of radius (r1)')
+        self.label8=QLabel('intrinsic impedance (z0')
+        self.tab5_layout.addWidget(self.label2,7,0)
+        self.tab5_layout.addWidget(self.label3,8,0)
+        self.tab5_layout.addWidget(self.label4,9,0)
+        self.tab5_layout.addWidget(self.label5,7,2)
+        self.tab5_layout.addWidget(self.label6,8,2)
+        self.tab5_layout.addWidget(self.label7,9,2)
+        self.tab5_layout.addWidget(self.label8,7,4)
+        self.textin2=QLineEdit("20")   # nl
+        self.textin3=QLineEdit("0.2")   # l0
+        self.textin4=QLineEdit("5.0")   # l1
+        self.textin5=QLineEdit("10")   # nr
+        self.textin6=QLineEdit("0.002")   # r0
+        self.textin7=QLineEdit("0.01")   # r1
+        self.textin8=QLineEdit("50.0")   # z0
+        self.tab5_layout.addWidget(self.textin2,7,1)
+        self.tab5_layout.addWidget(self.textin3,8,1)
+        self.tab5_layout.addWidget(self.textin4,9,1)
+        self.tab5_layout.addWidget(self.textin5,7,3)
+        self.tab5_layout.addWidget(self.textin6,8,3)
+        self.tab5_layout.addWidget(self.textin7,9,3)
+        self.tab5_layout.addWidget(self.textin8,7,5)
+        # set up button to trigger the action
+        self.but1=QPushButton('Click to Refresh Plot')
+        self.but1.setStyleSheet("font-size:20px;background-color:#FFFFFF")
+        self.but1.setFixedSize(400,25)
+        self.but1.clicked.connect(self.plotting)
+        self.tab5_layout.addWidget(self.but1,8,4,1,2) 
+        # set up progress bar     
+        self.progress=QProgressBar()
+        self.tab5_layout.addWidget(self.progress,9,4,1,2)
+        
+        # add tab5_layout to tab5
+        self.tab5.setLayout(self.tab5_layout)
+        # add tabs to layout1 and add layout1 to mainW1
+        self.layout1.addWidget(self.tabs)
+        self.layout1.setAlignment(Qt.AlignCenter)
+        self.mainW1.setLayout(self.layout1)
+        # adjust size and position of mainW1
+        self.mainW1.resize(1100,560)
+        self.mainW1.move(10,30)
+
+    @pyqtSlot()
+    def aboutact(self):
+        resp=QMessageBox.question(self,'About','This GUI is designed to demonstrate Navier-Stokes-based Channel Flow Analysis',QMessageBox.Ok)
+     
+    @pyqtSlot()
+    def plotting(self):
+        self.complete=0
+        while self.complete<100:
+           self.complete+=0.0001
+           self.progress.setValue(self.complete)
+        #collect values from textinputs adn assign to navierstokes.vars
+        self.fig.nl=int(self.textin2.text())
+        self.fig.l0=float(self.textin3.text())
+        self.fig.l1=float(self.textin4.text())
+        self.fig.nr=int(self.textin5.text())
+        self.fig.r0=float(self.textin6.text())
+        self.fig.r1=float(self.textin7.text())
+        self.fig.z0=float(self.textin8.text())
+        #recompute and redraw the plot       
+        self.fig.updatefig()   # update plots
+                                
+if __name__=="__main__":
+	qApp = QApplication(sys.argv)
+	aw = App()
+	aw.show()
+	sys.exit(qApp.exec_())
 
 
-#plt.plot(lengths, reflections)
-R, L = np.meshgrid(radii, lengths)
-fig=plt.figure(figsize=(11,7),dpi=100)
-ax=plt.subplot(111)
-c=ax.contourf(L, R, reflections, cmap=cm.coolwarm)
-plt.xlabel("Antenna length (m)")
-plt.ylabel("Radius (m)")
-plt.title("Reflection Coefficient vs Length and Radius")
-cb=plt.colorbar(c)
-plt.show()
---
+++ plot.py
@@ -0,0 +1,53 @@
+from necpp import *
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import gridspec
+
+# import user modules
+import impedance
+
+def compute(nl,nr,lengths,radii,z0):
+	reflections=np.zeros((nr,nl))
+	for i in range(nr):   # row for y axis
+	  for j in range(nl):  # column for x axis
+	    z = impedance.impedance(freq=134.5, base=0.5, length=lengths[j], radius=radii[i])
+	    reflections[i,j]=impedance.gamma(z,z0)
+	return reflections
+
+def plot(lengths, radii, reflections):
+	#plt.plot(lengths, reflections)
+	L, R = np.meshgrid(lengths, radii)
+	fig1=plt.figure(figsize=(10,4),dpi=100)
+	fig1.suptitle("Reflection Coefficient vs Length and Radius")
+	# set up the grid using gridspec
+	gs=gridspec.GridSpec(1, 2, width_ratios=[1,1.2])		
+	ax1=fig1.add_subplot(gs[0])
+	#c1=ax1.contourf(L, R, reflections, cmap=cm.jet)
+	c1=ax1.imshow(reflections, origin="lower", interpolation="bilinear", extent=[np.amin(lengths),np.amax(lengths),np.amin(radii),np.amax(radii)], cmap=cm.jet, aspect=(np.amax(lengths)-np.amin(lengths))/(np.amax(radii)-np.amin(radii)) )
+	# shift position of 1st colobar	
+	box1=ax1.get_position()
+	axcb1=plt.axes([box1.x0*1.02 + box1.width * 1.02, box1.y0, 0.01, box1.height])
+	cb1=plt.colorbar(c1, cax=axcb1)	
+	ax1.set_xlabel("Antenna length (m)")
+	ax1.set_ylabel("Radius (m)")
+	#ax1.set_title("2D")
+	ax2=fig1.add_subplot(gs[1],projection="3d")
+	ax2.view_init(50,130)
+	s2=ax2.plot_surface(L, R, reflections, cmap=cm.jet)
+	# shift position of 2nd colobar
+	box2=ax2.get_position()
+	axcb2=plt.axes([box2.x0*1.05 + box2.width * 1.05, box2.y0, 0.01, box2.height])
+	cb2=plt.colorbar(s2, cax=axcb2)
+	ax2.set_xlabel("Antenna length (m)")
+	ax2.set_ylabel("Radius (m)")
+	ax2.set_zlabel("Reflection Coefficient")
+	#ax2.set_title("3D")
+	return fig1, ax1, ax2, cb1, cb2, axcb1, axcb2
+	
+if __name__=="__main__":
+	reflections=compute(nl,nr,lengths,radii,z0)
+	plot(lengths,radii,reflections)
+	plt.show()
